8275be424f26e3f4c849f0ddc4166077b04838d9
Quentin <quentin.leveque@bluebotics.com>
2020-04-16 22:18:59 +0200

    maj for support team

--- README.md
+++ README.md
@@ -4,6 +4,74 @@
 
 ### Installation:
 ```
-pip3 install -r requirements.txt
 sudo python3 setup.py install
 ```
+
+### Configure
+Configure your account
+```
+tt set_user <USERNAME>
+tt set_password <PASSWORD>
+```
+and you are good to go.
+
+### Standard workflow
+You arrive at BBx, and you start working on the project "ABC". You even want to be more precise: you add the note "the notes of the task".
+You can then use `tt` as follow:
+```
+tt start ABC "the notes of the task"
+```
+"ABC" may be ambiguous, you may select a specific project within a match list.
+To ensure your work had been properly set, you can run
+```
+tt show
+```
+An hour goes by. You start a new task: the "DEF" task. This time, you don't need any note on that task. So you can just type:
+```
+tt start DEF
+```
+Then, it's lunch time!
+```
+tt stop
+```
+You start working again at 13:00. But you forgot to report it using tt, and it is already 14:30.
+No problem, you can just use the `-s` parameter:
+```
+tt start DEF "after lunch work" -s 13:00
+```
+You relealize you did not start at 13:00, but rather at 13:30, because you were playing table tennis.
+No worries, you can just run
+```
+tt cancel
+tt start DEF "after lunch work" -s 13:30
+```
+And it is already the end of the day...
+```
+tt stop -s 18:00
+```
+
+### Useful commands
+```
+tt day
+tt year
+tt overtime
+tt holiday
+```
+
+### How to be even more efficient
+You can also specify a default work on tt:
+```
+tt set_work MY_PROJECT "this text will always be the same day after day..."
+```
+Once you have done that, "MY_PROJECT" will be considered as your default project. So you no longer need to specify it when working on that project:
+```
+tt start
+```
+
+### Default works
+I don't want you to struggle with daily/weekly meetings. This is what I created a predefined project list. You can print it with:
+```
+tt projects
+```
+They also have predefined notes, so you don't need to bother rewriting the notes.
+
--- tt/tt.py
+++ tt/tt.py
@@ -1,13 +1,12 @@
 import numpy as np
 import requests
-import sys
 import json
 import os
 import time
-import click
+import argparse
 import re
+from collections import defaultdict
 from datetime import datetime, timedelta
-
 
 HOME_PATH = os.path.expanduser('~')
 CONFIG_PATH = os.path.join(HOME_PATH,'.config')
@@ -46,41 +45,63 @@ PUBLIC_HOLIDAY = ["2020-01-01",
                   "2020-12-25",
                   ]
 
-class Project:
-    def __init__(self, project, task, note = ""):
+
+class Task:
+    def __init__(self, project_number, task_number, project, task="", note="", start=""):
+        self.project_number = project_number
+        self.task_number = task_number
         self.project = project
         self.task = task
         self.note = note
+        self.start = start
 
-PROJECT_MAP = {
-    "scrum": Project(68, 2, "SCRUM meeting"),
-    "al": Project(1, 2),
-    "as": Project(53, 2),
-    "ant": Project(50, 2),
-    "tactical": Project(3, 1, "Tactical meeting"),
-    "man": Project(3, 1),
-    "rd": Project(68, 1)
-}   
+    @staticmethod
+    def from_dict(d):
+        if d.get("project_number") is None:
+            return None
+        all_attributes = ["project_number", "task_number", "project", "task", "note", "start"]
+        return Task(**{attr: d.get(attr, "") for attr in all_attributes})
+
+    def to_str(self):
+        r = self.project
+        if self.task:
+            r += ', ' + self.task
+        if self.note:
+            r += ': "{}"'.format(self.note)
+        if self.start:
+            r += " ({})".format(self.start)
+        return r
+
+
+TASK_MAP = {
+    "scrum": Task(68, 2, 'scrum', note='SCRUM meeting'),
+    "tactical": Task(3, 1, 'tactical', note='Tactical meeting'),
+    "al": Task(1, 2, 'al', note='ANT lab'),
+    "as": Task(53, 2, 'as', note='ANT server'),
+    "ant": Task(50, 2, 'ant', note='ANT'),
+    "man": Task(3, 1, 'man', note='Management'),
+    "rd": Task(68, 1, 'rd', note='R&D')
+}
+
 
 def remaining(data):
     if data is None:
         return 0
-    t = data.get("time", None)
-    if time is None:
+    start = data.get("start", None)
+    if start is None:
         return 0
-    tdelta = datetime.strptime(NOW, FMT) - datetime.strptime(t, FMT)
+    tdelta = datetime.strptime(NOW, FMT) - datetime.strptime(start, FMT)
     r = tdelta.total_seconds()/(60*60)
     return r
 
-def login(data):
-    config_data = load_config()
+
+def login(config_data):
     password = config_data.get("password", None)
     user = config_data.get("user", None)
         
     if password is None or user is None:
-        print("error credentials")
-        return
-        
+        exit("Error credentials")
+
     login_data = {
         "login": user,
         "password": password,
@@ -88,11 +109,15 @@ def login(data):
         "browser_today": TODAY
     }
 
-    r = requests.post(URL + "login.php", data=login_data)
+    try:
+        r = requests.post(URL + "login.php", data=login_data)
+    except:
+        exit('cannot connect')
     return r.cookies
 
-def get_hour(data, from_, to_):
-    cookies = login(data)
+
+def get_hour(config, from_, to_):
+    cookies = login(config)
     submit_data = {
         "favorite_report": -1,
         "project": None,
@@ -112,7 +137,10 @@ def get_hour(data, from_, to_):
         "btn_generate": "Generate",
         "fav_report_changed": None
     }
-    r = requests.post(URL + "reports.php", data=submit_data, cookies = cookies)
+    try:
+        r = requests.post(URL + "reports.php", data=submit_data, cookies = cookies)
+    except:
+        exit('cannot connect')
     format = '<td nowrap class="cellRightAlignedSubtotal">(\\d+):(\\d+)</td>'
     m = re.search(format, r.text)
     try:
@@ -121,164 +149,210 @@ def get_hour(data, from_, to_):
         return 0
     return hours + minutes/60.0
 
-def get_days_off():
-    config_data = load_config()
-    return config_data.get("days_off", [])
 
-def add_to_tt(data, t):
-    cookies = login(data)
+def choose_project(config, match):
+    cookies = login(config)
+    try:
+        r = requests.get(URL + "time.php", cookies=cookies)
+    except:
+        exit('cannot connect')
+    project_names = {}
+    task_ids = {}
+    task_names = {}
+    for line in r.text.split('\n'):
+        line = line.strip()
+        m = re.match('^([\\w_]+)\\[(\\d+)] = ".*";$', line)
+        if m:
+            exec(line)
+
+    results = []
+    for idx, name in project_names.items():
+        if re.findall(match, name, re.IGNORECASE):
+            tasks = task_ids[idx]
+            for task in tasks.split(','):
+                results.append(Task(idx, int(task), name, task_names[int(task)]))
+
+    if not results:
+        exit("No matching project")
+
+    for i, result in enumerate(results):
+        print('{} -> {}: {}'.format(i, result.project, result.task))
+
+    choice = input('Choose a project: ')
+
+    try:
+        return results[int(choice)]
+    except:
+        exit("No matching project")
+
+
+def get_project(config, option, start, note):
+    if not option:
+        project = Task.from_dict(config.get("saved_work", {}))
+        if project is None:
+            exit("No default work")
+    elif option in TASK_MAP:
+        project = TASK_MAP[option]
+    else:
+        project = choose_project(config, option)
+
+    if note:
+        project.note = note
+    project.start = start
+    return project
+
+
+def add_to_tt(config, data, t):
+    cookies = login(config)
+    project = Task.from_dict(data)
+    if project is None:
+        exit("No current project")
     submit_data = {
-        "project": data["project"],
-        "task": data["task"],
-        "start": data["time"],
+        "project": project.project_number,
+        "task": project.task_number,
+        "start": project.start,
         "finish": t,
         "date": TODAY,
-        "note": data["note"],
+        "note": project.note,
         "btn_submit": "Submit",
         "browser_today": TODAY
     }
-    
-    r = requests.post(URL + "time.php", data=submit_data, cookies = cookies)
 
-def save_config(key, value):
-    config_data = {}
-    if os.path.isfile(CONFIG_FILE):
-        with open(CONFIG_FILE, "r", encoding = "utf-8") as file:
-            config_data = json.load(file)
-    config_data[key] = value
+    try:
+        r = requests.post(URL + "time.php", data=submit_data, cookies=cookies)
+    except:
+        exit('cannot connect')
+
+
+def save_config(new_config):
     with open(CONFIG_FILE, 'w', encoding="utf-8") as file:
-        json.dump(config_data, file)
-      
+        json.dump(new_config, file)
+
+
 def load_config():
+    config_data = defaultdict(lambda: {})
     if os.path.isfile(CONFIG_FILE):
-            with open(CONFIG_FILE, "r", encoding = "utf-8") as file:
-                config_data = json.load(file)
-                return config_data
-    return None
+        with open(CONFIG_FILE, "r", encoding="utf-8") as file:
+            config_data.update(json.load(file))
+            return config_data
+    return config_data
 
-def save_data(new_data):
-    with open(SAVE_FILE, 'w', encoding="utf-8") as file:
-        json.dump(new_data, file)
-        
-def load_data():
-    if os.path.isfile(SAVE_FILE):
-            with open(SAVE_FILE, "r", encoding = "utf-8") as file:
-                config_data = json.load(file)
-                return config_data
-    return None
-        
-def remove_data():
-    os.remove(SAVE_FILE)
 
-@click.command()
-@click.argument('command', required=True)
-@click.argument('option', required=False)
-@click.argument('note', required=False, default="")
-@click.option('--start', '-s', help='Start at', default=None)
-def main(command, option, note, start):
+def remove_data(config):
+    if 'data' in config:
+        del config['data']
+        save_config(config)
+
+
+def main():
     """
-    Existing commands: stop, start, cancel, show, set_work, set_password, set_user, year, day
-    Existing projects: scrum, al, as, ant, tactical, man, rd
+    Existing commands:
+    stop, start, cancel, show, set_work, set_password, set_user, year, day, list,
+    overtime (ot), public-holiday (public), projects
     """
-    data = load_data()
+
+    # Parsing the data
+    parser = argparse.ArgumentParser(description=main.__doc__)
+    parser.add_argument('command', type=str)
+    parser.add_argument('option', type=str, nargs='?')
+    parser.add_argument('note', type=str, nargs='?')
+    parser.add_argument('-s', '--start',
+                        metavar='sarting_time',
+                        help='Set the starting time of the action',
+                        type=str,
+                        default=None)
+    args = parser.parse_args()
+
+    command = args.command
+    note = args.note
+    option = None if not args.option else args.option
+
     config = load_config()
-    hour = start
-    
-    if hour is None:
-        hour = NOW
-       
+    data = config.get('data', None)
+    start = args.start
+
+    if start is None:
+        start = NOW
+
     # command stop
     if command == "stop":
-        if data is not None:
-            add_to_tt(data, hour)
-            remove_data()
-        else:
-            print("error 1")
+        if data is None:
+            exit("You are not currently working")
+        add_to_tt(config, data, start)
+        remove_data(config)
 
     # command start
     elif command == "start":
         if data is not None:
-            add_to_tt(data, hour)
-        
-        if not option and config is not None:
-            option = config.get("work_proj", "")
-            note = config.get("work_desc", "")
-        
-        if option in PROJECT_MAP:
-            proj = PROJECT_MAP[option]
-            if not note:
-                note=proj.note
-            new_data = {
-                "command": option,
-                "project": proj.project,
-                "task": proj.task,
-                "time": hour,
-                "note": note
-            }
-            save_data(new_data)
-        else:
-            print("error 2")
-    
+            add_to_tt(config, data, start)
+        project = get_project(config, option, start, note)
+        config['data'] = project.__dict__
+        save_config(config)
+
     elif command == "cancel":
-        remove_data()
-    
+        remove_data(config)
+
     elif command == "show":
         if data is not None:
-            print("Current work: {} {} {}".format(data.get("command",""),
-                                                  data.get("time", ""),
-                                                  data.get("note", "")))
+            project = Task.from_dict(data)
+            print("Current work: {}".format(project.to_str()))
         else:
             print("No current work.")
             
-        if config is not None:
-            print("Current default work: {} {}".format(config.get("work_proj",""),
-                                                       config.get("work_desc","")))
-            print("Current user: {}".format(config.get("user","")))
+        project = Task.from_dict(config.get("saved_work", {}))
+
+        if project is not None:
+            print("Current default work: {}".format(project.to_str()))
         else:
-            print("No current user.")
-        
+            print("No current default work.")
+
+        print("Current user: {}".format(config.get("user", "None")))
+
     elif command == "set_work":
-        save_config("work_proj", option)
-        save_config("work_desc", note)
+        project = get_project(config, option, start, note)
+        config["saved_work"] = project.__dict__
+        save_config(config)
     
     elif command == "set_password":
-        save_config("password", option)
+        config["password"] = option
+        save_config(config)
         
     elif command == "set_user":
-        save_config("user", option)
+        config["user"] = option
+        save_config(config)
         
     elif command == "year":
         if not option:
             option = YEAR
         from_ = "{}-01-01".format(str(option))
         to_ = "{}-12-31".format(str(option))
-        tot = get_hour(data, from_, to_)
+        tot = get_hour(config, from_, to_)
         tot += remaining(data)
         print(tot)
     
     elif command == "day":
         if not option:
             option = TODAY
-        tot = get_hour(data, option, option)
+        tot = get_hour(config, option, option)
         tot += remaining(data)
         print(tot)
 
     elif command == "overtime" or command == "ot":
-        holidays = PUBLIC_HOLIDAY + get_days_off()
+        holidays = PUBLIC_HOLIDAY + config.get("days_off", [])
         working_day = np.busday_count(YEAR, TODAY, weekmask='1111100', holidays=holidays)
         working_hours = working_day * HOURS_PER_DAY
 
         from_ = "{}-01-01".format(str(YEAR))
         yesterday = (datetime.now() - timedelta(1)).strftime(DATE_FMT)
-        worked_hours = get_hour(data, from_, yesterday)
+        worked_hours = get_hour(config, from_, yesterday)
 
-        worked_hours_today = get_hour(data, TODAY, TODAY) + remaining(data)
+        worked_hours_today = get_hour(config, TODAY, TODAY) + remaining(data)
         worked_hours += worked_hours_today
 
         if worked_hours_today < HOURS_PER_DAY:
             working_hours += worked_hours_today
-            should_work_until = datetime.strptime(NOW, FMT) \
-                                + timedelta(hours=(HOURS_PER_DAY - worked_hours_today))
+            should_work_until = (datetime.strptime(NOW, FMT) +
+                                 timedelta(hours=(HOURS_PER_DAY - worked_hours_today)))
             print("Your day is not done, you should work until",
                   datetime.strftime(should_work_until, FMT))
         else:
@@ -300,7 +374,7 @@ def main(command, option, note, start):
         print(PUBLIC_HOLIDAY)
 
     elif command == "holiday":
-        off = get_days_off()
+        off = config.get("days_off", [])
         off_nb = len(off)
         if not off_nb:
             print("You have no holidays registered in TT, add them in {} if needed"
@@ -311,8 +385,13 @@ def main(command, option, note, start):
         print("You have {} days left !".format(max(0, left)))
         print("Here is what you have used:\n{}".format(off))
 
+    elif command == "projects":
+        for name, value in TASK_MAP.items():
+            print(value.to_str())
+
     else:
-        print("error 3")    
+        exit("Unknown command")
+
 
 if __name__ == "__main__":
     main()
